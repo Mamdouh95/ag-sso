9debdfd7576619396bc805bf003580cdd0b64cac
Mamdouh Issa <mamdouh.issa@agriserv.sa>
2024-11-27 08:36:50 +0300

    SSO login / callback 2 different methods.

--- src/Http/Controllers/SsoController.php
+++ src/Http/Controllers/SsoController.php
@@ -9,45 +9,51 @@ use Illuminate\Support\Facades\Log;
 
 class SsoController extends Controller
 {
-    public function index(Request $request)
+    public function login(Request $request)
     {
         try {
-            if ($request->has('token')) {
+            // Encrypt the redirect URI
+            $encrypter = new Encrypter(config('sso.secret_key'), strtolower(config('app.cipher')));
 
-                // Fetch user info from SSO
-                $userInfo = Http::acceptJson()
-                    ->contentType('application/json')
-                    ->get(config('sso.base_uri') . '/api/auth/sso/user', [
-                        'token' => $request->input('token')
-                    ])
-                    ->json();
+            $token = $encrypter->encryptString(config('sso.redirect_uri'));
 
-                if (!isset($userInfo['service']['items'])) {
-                    throw new \Exception("Invalid SSO response format.");
-                }
+            // Prepare redirect URL
+            $redirectUrl = config('sso.base_uri') . '?' . http_build_query([
+                    'sso' => config('sso.id'),
+                    'token' => $token,
+                ]);
 
-                $this->handleUserInfo($userInfo['service']['items']);
+            return redirect($redirectUrl);
 
-                return redirect()->to('/');
-
-            } else {
-                // Encrypt the redirect URI
-                $encrypter = new Encrypter(config('sso.secret_key'), strtolower(config('app.cipher')));
-
-                $token = $encrypter->encryptString(config('sso.redirect_uri'));
-
-                // Prepare redirect URL
-                $redirectUrl = config('sso.base_uri') . '?' . http_build_query([
-                        'sso' => config('sso.id'),
-                        'token' => $token,
-                    ]);
-
-                return redirect($redirectUrl);
-            }
         } catch (\Exception $exception) {
+
             Log::error("SSO Login Error: " . $exception->getMessage());
+
             return redirect()->to('/')->withErrors(['sso_error' => 'Single sign-on failed. Please try again.']);
         }
+    }
+
+    public function callback(Request $request)
+    {
+        // Fetch user info from SSO
+        if ($request->has('token')) {
+            throw new \Exception("Token not found.");
+        }
+
+        $userInfo = Http::acceptJson()
+            ->contentType('application/json')
+            ->get(config('sso.base_uri') . '/api/auth/sso/user', [
+                'token' => $request->input('token')
+            ])
+            ->json();
+
+        if (!isset($userInfo['service']['items'])) {
+            throw new \Exception("Invalid SSO response format.");
+        }
+
+        $this->handleUserInfo($userInfo['service']['items']);
+
+        return redirect()->to('/');
     }
 
     /**
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Agriserv\SSO\Http\Controllers\SsoController;
 
 Route::group(['middleware' => 'web'], function () {
-    Route::get('auth/sso', [LoginSsoController::class, 'index'])->name('auth.sso');
+    Route::get('auth/sso', [LoginSsoController::class, 'login'])->name('auth.sso');
+    Route::get('auth/callback', [LoginSsoController::class, 'callback'])->name('auth.callback');
     Route::post('auth/logout', [SsoController::class, 'logout'])->name('auth.logout.store');
 });
